1cc7b3668488ed11621651cf9ade789e9b51948f
Nati Pinyan <Natipinyan@gmail.com>
2025-07-22 20:47:11 +0300

    add db config

---
+++ db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+
+global.db_pool = mysql.createPool({
+    host:		process.env.HOST	,
+    user:		process.env.USER	,
+    password:	process.env.PASSWORD	,
+    database:	process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+})
+
+module.exports = {
+    db_pool:db_pool
+};
