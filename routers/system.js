297e70fb455471bd0074310b964b901b6c8ac3b2
Nati Pinyan <Natipinyan@gmail.com>
2025-07-24 01:22:41 +0300

    Deleting unnecessary routers

--- routers/system.js
+++ routers/system.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const user_Mid = require('../middleware/user_Mid');
 
-router.get('/register', (req, res) => {
-    res.render('register', {});
-});
-
-router.post('/register', user_Mid.AddUser, (req, res) => {});
-
 router.get('/login', (req, res) => {
     res.render('login', {});
 });
